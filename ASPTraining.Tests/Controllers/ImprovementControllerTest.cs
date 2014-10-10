9b560b57061c2635b09a64dc67608f77b8ca0c85
Tom Howlett <tom@tomhowlett.com>
2014-10-10 17:21:47 +0100

    Added some language tweaks

--- ASPTraining.Tests/Controllers/ImprovementControllerTest.cs
+++ ASPTraining.Tests/Controllers/ImprovementControllerTest.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Web.Mvc;
-using ASPTraining.Controllers;
-using Moq;
+﻿using ASPTraining.Controllers;
 using ASPTraining.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Web.Mvc;
 
 namespace ASPTraining.Tests.Controllers
 {
@@ -16,6 +15,7 @@ namespace ASPTraining.Tests.Controllers
         [TestMethod]
         public void Index_ShouldOrderImprovementsByStatus()
         {
+            //object initialisation and lists
             var data = new List<Improvement>(){
                 new Improvement(){ Id=1, Description= "Hi", Status = Status.InProgress},
                 new Improvement(){ Id=2, Description= "Low", Status = Status.Done},
@@ -26,6 +26,7 @@ namespace ASPTraining.Tests.Controllers
                 .SetupData(data);
 
             var context = new Mock<ApplicationDbContext>();
+            //lambda
             context.Setup(c => c.Improvements).Returns(set.Object);
 
             // Arrange
@@ -36,9 +37,38 @@ namespace ASPTraining.Tests.Controllers
 
             // Assert
             var improvements = result.Model as List<Improvement>;
+
+
             Assert.AreEqual(Status.ToDo, improvements[0].Status);
             Assert.AreEqual(Status.InProgress, improvements[1].Status);
             Assert.AreEqual(Status.Done, improvements[2].Status);
+
+            //Optional parameters
+        }
+
+        [TestMethod]
+        public void TestOptionalAndAnonymous()
+        {
+            //string result = Foo(b: "c");
+            //var result =  Foo("f");
+            var result = Foo(null, "f");
+
+            Assert.Fail(result);
+            
+        }
+
+        private string Foo(string a = "a", string b = "b")
+        {
+            return new { a, b }.ToString();
+        }
+
+    }
+
+    public static class StringExtensions
+    {
+        public static string F(this string input, params string[] args)
+        {
+            return String.Format(input, args);
         }
     }
 }
