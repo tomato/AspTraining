1da8a4a95388dd56360979dc6739650251243273
Tom Howlett <tom@tomhowlett.com>
2014-10-10 10:42:19 +0100

    Initial Commit

---
+++ ASPTraining/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPTraining.Startup))]
+namespace ASPTraining
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
